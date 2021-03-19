989bb57a26042dd1e0077a9a0d5fc72b0567846f
xxyusaku <premiumhome0yy@gmail.com>
2021-03-19 17:42:17 +0900

    1

---
+++ src/components/CommonFooter.vue
@@ -0,0 +1,45 @@
+<template>
+  <footer class="top-footer">
+    <div class="footer-bar">
+      <div class="flex">
+        <h1 class="header-logo">estra inc.</h1>
+        <nav class="header-nav">
+          <ul>
+            <li><a href="#">home</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">service</a></li>
+            <li><a href="#">works</a></li>
+            <li><a href="#">price</a></li>
+            <li><a href="#">contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <small>Copyright 2019 estra inc. All Rights Reserved.</small>
+  </footer>
+</template>
+
+<style scoped>
+.footer-bar {
+  height: 70px;
+  line-height: 70px;
+}
+
+.top-footer {
+  background: #000;
+  text-align: center;
+}
+
+.top-footer small {
+  text-align: center;
+  color: #fff;
+}
+
+@media screen and (max-width: 768px) {
+  .top-footer .header-logo {
+    margin: 0;
+    width: 100%;
+    text-align: center;
+  }
+}
+</style>
