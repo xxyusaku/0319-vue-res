12095e423a53f47e979053910a3c53f06fb49277
xxyusaku <premiumhome0yy@gmail.com>
2021-03-20 10:05:57 +0900

    2

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -1,31 +1,26 @@
 <template>
   <div id="firstview">
-      <header id="top-header">
-        <div class="flex">
-          <h1 class="header-logo">estra inc.</h1>
-          <nav class="header-nav">
-            <ul>
-              <li><a href="#">home</a></li>
-              <li><a href="#">about</a></li>
-              <li><a href="#">service</a></li>
-              <li><a href="#">works</a></li>
-              <li><a href="#">price</a></li>
-              <li><a href="#">contact</a></li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-      <p class="catchcopy">機械ではできない仕事を</p>
-      <div class="news flex">
-        <div class="news-content">
-          <p class="news_date">2019/01/01</p>
-          <p class="news_title">
-            ニュースタイトルがここには入ります。3月の決算報告書
-          </p>
-        </div>
-        <div class="news-icon">news</div>
+    <header id="top-header">
+      <div class="flex">
+        <h1 class="header-logo">estra inc.</h1>
+        <nav class="header-nav">
+          <ul>
+            <li><a href="#">home</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">service</a></li>
+            <li><a href="#">works</a></li>
+            <li><a href="#">price</a></li>
+            <li><a href="#">contact</a></li>
+          </ul>
+        </nav>
       </div>
+    </header>
+    <div id="hamburger">
+      <span></span>
+      <span></span>
+      <span></span>
     </div>
+  </div>
 </template>
 
 <style scoped>
@@ -75,55 +70,4 @@
   color: #fff;
   line-height: 1;
 }
-
-@media screen and (max-width: 768px) {
-  .header-nav {
-    display: none;
-  }
-  .catchcopy {
-    top: 50%;
-    left: 50%;
-    width: 80%;
-    text-align: center;
-    transform: translate(-50%, -50%);
-  }
-
-  #hamburger {
-    width: 40px;
-    height: 40px;
-    position: absolute;
-    cursor: pointer;
-    z-index: 100;
-    top: 15px;
-    right: 20px;
-    transition: 0.4s;
-    display: block;
-  }
-  #hamburger span {
-    display: block;
-    width: 35px;
-    height: 2px;
-    background-color: #fff;
-    transition: 0.8s;
-  }
-  #hamburger span:nth-of-type(1) {
-    position: absolute;
-    top: 10px;
-    left: 2px;
-  }
-  #hamburger span:nth-of-type(2) {
-    position: absolute;
-    top: 20px;
-    left: 2px;
-  }
-  #hamburger span:nth-of-type(3) {
-    position: absolute;
-    top: 30px;
-    left: 2px;
-  }
-}
-
-
-
-
 </style>
